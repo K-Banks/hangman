57b6100116cba7ee1626e28001be77bde8ac9474
Kayl Eubanks and Matt Miller <matt@>
2018-05-23 10:36:46 -0700

    Pass test for adding character to guess array.

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -49,6 +49,7 @@ public class Game {
     }
 
     public List<Character> addCharacterToGuesses(Character guessedCharacter) {
+        guessArray.add(guessedCharacter);
         return guessArray;
     }
 }
